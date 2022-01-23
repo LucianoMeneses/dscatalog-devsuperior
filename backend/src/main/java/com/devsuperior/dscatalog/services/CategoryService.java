9b14c8db34c6c3f665847c18b7d17b5c315d64dd
LUCIANO ARAUJO MENESES - GETIC - GETIC <214083@int.cagece.com.br>
2022-01-23 19:17:09 -0300

    Database Seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -2,6 +2,8 @@ package com.devsuperior.dscatalog.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.devsuperior.dscatalog.categories.CategoryRepository;
 import com.devsuperior.dscatalog.entities.Category;
 
@@ -11,6 +13,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 		
